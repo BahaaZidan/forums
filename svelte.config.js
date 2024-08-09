7e9cde2e1f72d890735ded845128e1d939428cca
BahaaZidan <gebnatorky@gmail.com>
2024-08-09 16:21:06 +0300

    adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// TODO: make it compatible with edge
+			runtime: 'nodejs20.x',
+			regions: ['iad1']
+		})
 	}
 };
 
