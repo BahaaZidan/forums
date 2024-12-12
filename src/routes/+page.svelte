facc59a07ea1aca0bcc2e4569df660f3b8159074
BahaaZidan <gebnatorky@gmail.com>
2024-12-12 12:02:00 +0200

    agian

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import type { PageData } from './$types';
 
 	interface Props {
@@ -11,9 +10,3 @@
 
 <h1>forums-tenant</h1>
 <p>{data.hostname}</p>
-
-<h1>Hi, {data.user.username}!</h1>
-<p>Your user ID is {data.user.id}.</p>
-<form method="post" use:enhance>
-	<button>Sign out</button>
-</form>
