b22eae38775e8d49895729bf2e678c747696ef0c
BahaaZidan <gebnatorky@gmail.com>
2024-12-12 12:00:10 +0200

    get it to compile

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import '../app.css';
-	/** @type {{children?: import('svelte').Snippet}} */
-	let { children } = $props();
-</script>
-
-{@render children?.()}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,30 +1,7 @@
-import { fail, redirect } from '@sveltejs/kit';
-import type { Actions, PageServerLoad } from './$types';
-import { createDB } from '$lib/server/db';
-import { createLucia } from '$lib/server/auth';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-	if (!event.locals.user) return redirect(302, '/login');
-
 	return {
-		user: event.locals.user,
 		hostname: event.url.hostname
 	};
-};
-
-export const actions: Actions = {
-	default: async (event) => {
-		if (!event.locals.session) {
-			return fail(401);
-		}
-		const db = await createDB(event.url.hostname);
-		const lucia = createLucia(db);
-		await lucia.invalidateSession(event.locals.session.id);
-		const sessionCookie = lucia.createBlankSessionCookie();
-		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-			path: '.',
-			...sessionCookie.attributes
-		});
-		return redirect(302, '/login');
-	}
 };
